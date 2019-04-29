c511295a620db197481a788ba0cffbf13ca8a6e7
raigarrr <2157628@slu.edu.ph>
2019-04-29 14:47:23 +0800

    other unit and employee queries

---
+++ database/employee.sql
@@ -0,0 +1,8 @@
+use marthagoshen;
+
+select * from employee;
+
+INSERT INTO 
+	`marthagoshen`.`employee` (`email`, `firstname`, `lastname`, `position`) 
+VALUES 
+	('manongjuan@marthaservices.com', 'juan', 'cruz', 'maintenance');
---
+++ database/unit.sql
@@ -0,0 +1,6 @@
+use marthagoshen;
+
+select * from unit;
+
+INSERT INTO `marthagoshen`.`unit` (`unitid`, `building`, `status`) VALUES ('5LE-E', 'harvard', 'open'); 
+
