213869f57000c6abf96068be03318d88ecd4d6b2
ferdzdeAsis <32946437+ferdzdeAsis@users.noreply.github.com>
2019-05-13 14:33:56 +0800

    Pa-populate ung mga tables

--- database/marthagoshen.sql
+++ database/marthagoshen.sql
@@ -1,63 +1,67 @@
--- MySQL dump 10.13  Distrib 8.0.16, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: marthagoshen
--- ------------------------------------------------------
--- Server version	5.7.24
+-- Host: 127.0.0.1:3306
+-- Generation Time: May 13, 2019 at 06:29 AM
+-- Server version: 5.5.60
+-- PHP Version: 7.3.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
- SET NAMES utf8 ;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `marthagoshen`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `billing`
 --
 
 DROP TABLE IF EXISTS `billing`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `billing` (
-  `billid` int(10) NOT NULL,
-  `date` timestamp NOT NULL,
+CREATE TABLE IF NOT EXISTS `billing` (
+  `billid` varchar(10) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `duedate` datetime NOT NULL,
   `totalamount` int(10) NOT NULL,
   `type` enum('rent','utility','maintenance','') NOT NULL,
-  `empid` int(11) NOT NULL,
-  `unit` int(10) NOT NULL,
+  `empid` varchar(11) NOT NULL,
+  `unitId` varchar(10) NOT NULL,
   `building` enum('harvard','princeton','wharton','') NOT NULL,
   PRIMARY KEY (`billid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `billing`
 --
 
-LOCK TABLES `billing` WRITE;
-/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
-/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `billing` (`billid`, `date`, `duedate`, `totalamount`, `type`, `empid`, `unitId`, `building`) VALUES
+('B001', '2019-05-13 06:27:16', '2019-06-10 17:00:00', 1245, 'rent', 'EP02', '5LM-E', 'princeton');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `concern`
 --
 
 DROP TABLE IF EXISTS `concern`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `concern` (
-  `concernid` int(10) NOT NULL,
-  `reporteddate` timestamp NOT NULL,
+CREATE TABLE IF NOT EXISTS `concern` (
+  `concernid` varchar(10) NOT NULL,
+  `reporteddate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `description` varchar(70) NOT NULL,
-  `timeavailable` timestamp NOT NULL,
+  `timeavailable` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `status` enum('done','processing') NOT NULL,
-  `dateavailable` timestamp NOT NULL,
+  `dateavailable` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `total` int(10) DEFAULT NULL,
   `id` int(8) NOT NULL,
   `response` varchar(70) NOT NULL,
@@ -66,56 +70,53 @@ CREATE TABLE `concern` (
   KEY `id` (`id`),
   KEY `id_2` (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `concern`
 --
 
-LOCK TABLES `concern` WRITE;
-/*!40000 ALTER TABLE `concern` DISABLE KEYS */;
-/*!40000 ALTER TABLE `concern` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `concern` (`concernid`, `reporteddate`, `description`, `timeavailable`, `status`, `dateavailable`, `total`, `id`, `response`, `feedback`) VALUES
+('CO1', '2019-05-13 03:59:40', 'broken door', '2019-09-18 04:30:00', 'processing', '2019-10-26 16:00:00', NULL, 1, 'In the process of repairing', '');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `employee`
 --
 
 DROP TABLE IF EXISTS `employee`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `employee` (
-  `empid` int(7) NOT NULL,
-  `email` varchar(30) NOT NULL,
-  `firstname` char(30) NOT NULL,
-  `lastname` char(20) NOT NULL,
-  `contact` int(11) DEFAULT NULL,
+CREATE TABLE IF NOT EXISTS `employee` (
+  `empid` varchar(7) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `firstname` char(50) NOT NULL,
+  `lastname` char(50) NOT NULL,
+  `contact` varchar(11) DEFAULT NULL,
   `position` enum('admin','leasing officer','leasing manager','accounting officer','accounting manager','maintenance') NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`empid`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `employee`
 --
 
-LOCK TABLES `employee` WRITE;
-/*!40000 ALTER TABLE `employee` DISABLE KEYS */;
-/*!40000 ALTER TABLE `employee` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `employee` (`empid`, `email`, `firstname`, `lastname`, `contact`, `position`, `password`) VALUES
+('EP01', 'ferdinanddeasis@marthservices.com', 'Ferdinand II', 'De Asis', '09175057090', 'admin', 'ferdinand'),
+('EP02', 'abigail@marthaservices.com', 'Abigail', 'Rubrico', '09875261895', 'accounting officer', 'abigailrubrico'),
+('EP03', 'hazelignacio@marthaservices.com', 'Hazel', 'Ignacio', '09739486184', 'leasing manager', 'hazenacio'),
+('EP04', 'drevasuncion@marthaservices.com', 'Drev', 'Asuncion', '09785261846', 'leasing officer', 'drevesper');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `history`
 --
 
 DROP TABLE IF EXISTS `history`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `history` (
+CREATE TABLE IF NOT EXISTS `history` (
   `id` int(10) NOT NULL,
-  `time` timestamp NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `data` varchar(10) NOT NULL,
   `quantity` int(10) NOT NULL,
   `uniprice` int(6) NOT NULL,
@@ -123,26 +124,16 @@ CREATE TABLE `history` (
   `idemp` int(10) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `history`
---
-
-LOCK TABLES `history` WRITE;
-/*!40000 ALTER TABLE `history` DISABLE KEYS */;
-/*!40000 ALTER TABLE `history` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `item`
 --
 
 DROP TABLE IF EXISTS `item`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `item` (
-  `itemid` int(6) NOT NULL,
+CREATE TABLE IF NOT EXISTS `item` (
+  `itemid` varchar(10) NOT NULL,
   `item` varchar(50) NOT NULL,
   `brand` varchar(50) NOT NULL,
   `description` text,
@@ -152,105 +143,91 @@ CREATE TABLE `item` (
   UNIQUE KEY `item` (`item`),
   UNIQUE KEY `item_2` (`item`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `item`
 --
 
-LOCK TABLES `item` WRITE;
-/*!40000 ALTER TABLE `item` DISABLE KEYS */;
-/*!40000 ALTER TABLE `item` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `item` (`itemid`, `item`, `brand`, `description`, `quantity`, `unitprice`) VALUES
+('IT0001', 'doorknob', 'Medeco', 'doorknob with a lock', 1, 250);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `requesteditem`
 --
 
 DROP TABLE IF EXISTS `requesteditem`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `requesteditem` (
-  `concernid` int(10) NOT NULL,
-  `itemid` int(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS `requesteditem` (
+  `concernid` varchar(10) NOT NULL,
+  `itemid` varchar(10) NOT NULL,
   `quantity` int(10) NOT NULL,
   `total` int(10) NOT NULL,
   `unitprice` int(10) NOT NULL,
   PRIMARY KEY (`concernid`,`itemid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `requesteditem`
 --
 
-LOCK TABLES `requesteditem` WRITE;
-/*!40000 ALTER TABLE `requesteditem` DISABLE KEYS */;
-/*!40000 ALTER TABLE `requesteditem` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `requesteditem` (`concernid`, `itemid`, `quantity`, `total`, `unitprice`) VALUES
+('CO1', 'IT001', 3, 750, 250);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `tenant`
 --
 
 DROP TABLE IF EXISTS `tenant`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `tenant` (
-  `id` int(7) NOT NULL,
-  `email` int(50) NOT NULL,
-  `firstname` int(50) NOT NULL,
-  `lastname` int(10) NOT NULL,
-  `password` int(30) NOT NULL,
-  `contact` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `tenant` (
+  `id` varchar(7) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(10) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `contact` varchar(11) NOT NULL,
   `building` enum('harvard','princeton','wharton') NOT NULL,
   `unitid` varchar(8) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `tenant`
 --
 
-LOCK TABLES `tenant` WRITE;
-/*!40000 ALTER TABLE `tenant` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tenant` ENABLE KEYS */;
-UNLOCK TABLES;
+INSERT INTO `tenant` (`id`, `email`, `firstname`, `lastname`, `password`, `contact`, `building`, `unitid`) VALUES
+('T001', 'jeffpaswick@marthaservices.com', 'Jeff', 'Paswick', 'jeffpaswick', '09065792344', 'princeton', '5LM-E'),
+('T002', 'yvonnemanuel@marthaservices.com', 'Yvonne', 'Manuel', 'yvonnemanuel12', '09302406268', 'wharton', '5LA-W'),
+('T003', 'apostoljohn@marthaservices.com', 'John Raphael', 'Apostol', 'raphpostol', '09516782905', 'harvard', '3RS-B');
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `unit`
 --
 
 DROP TABLE IF EXISTS `unit`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
- SET character_set_client = utf8mb4 ;
-CREATE TABLE `unit` (
-  `unitid` varchar(6) NOT NULL,
+CREATE TABLE IF NOT EXISTS `unit` (
+  `unitId` varchar(6) NOT NULL,
   `building` enum('harvard','princeton','wharton','') NOT NULL,
   `owner` char(50) DEFAULT NULL,
   `status` enum('open','reserved','occupied') NOT NULL,
-  PRIMARY KEY (`unitid`)
+  PRIMARY KEY (`unitId`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `unit`
 --
 
-LOCK TABLES `unit` WRITE;
-/*!40000 ALTER TABLE `unit` DISABLE KEYS */;
-INSERT INTO `unit` VALUES ('5LM-E','harvard',NULL,'open'),('5LN-E','wharton',NULL,'open');
-/*!40000 ALTER TABLE `unit` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `unit` (`unitId`, `building`, `owner`, `status`) VALUES
+('5LM-E', 'princeton', 'Jeff Paswick', 'occupied'),
+('5LN-E', 'wharton', NULL, 'open'),
+('5LO-E', 'princeton', NULL, 'open'),
+('5LE-E', 'harvard', NULL, 'open');
+COMMIT;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-04-29  9:59:30
