094ef84628d7173fb6611761c15419c051e1aa64
ferdzdeAsis <32946437+ferdzdeAsis@users.noreply.github.com>
2019-06-18 21:33:45 +0800

    Delete employee.sql

--- Martha/employee.sql
+++
@@ -1,8 +0,0 @@
-use marthagoshen;
-
-select * from employee;
-
-INSERT INTO 
-	`marthagoshen`.`employee` (`email`, `firstname`, `lastname`, `position`) 
-VALUES 
-	('manongjuan@marthaservices.com', 'juan', 'cruz', 'maintenance');
